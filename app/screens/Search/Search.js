8813d079c2598bebe55b1b1d8e01e0cd0cd60102
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-11-11 16:57:52 -0800

    fix: switch place type

--- app/screens/Search/Search.js
+++ app/screens/Search/Search.js
@@ -5,9 +5,15 @@ import { SpotTypeCarousel, SpotItem, Map } from '../../components';
 import { setNearbySpots } from '../../redux/actions/SearchActions';
 import { getSelectedPlaceTypes } from '../../redux/actions/UserActions';
 
+const initialCoodinate = {
+  lat: 35.652832,
+  lng: 139.839478,
+}
+
 class Search extends Component {
   state = {
     placeTypeIdx: 0,
+    currentCoodinate: initialCoodinate
   }
 
   fetchNearbySpots = (coodinate, index) => {
@@ -33,14 +39,16 @@ class Search extends Component {
         <StatusBar barStyle={'light-content'} />
         <SpotTypeCarousel
           didChangeItem={index => {
-            this.fetchNearbySpots(index);
+            const { currentCoodinate = initialCoodinate } = this.state;
+            this.fetchNearbySpots(currentCoodinate, index);
             this.setState({ placeTypeIdx: index })
           }}
         />
-        <Map onChangeRegion={(region) => {
-          console.log('[##] region is ', region)
+        <Map onChangeRegion={(coodinate) => {
+          console.log('[##] region is ', coodinate)
           const { placeTypeIdx = 0 } = this.state;
-          this.fetchNearbySpots(region, placeTypeIdx);
+          this.fetchNearbySpots(coodinate, placeTypeIdx);
+          this.setState({ currentCoodinate: coodinate })
         }}/>
         {spots && (
           <FlatList data={spots} renderItem={this.renderListItem} keyExtractor={item => item.id} />
